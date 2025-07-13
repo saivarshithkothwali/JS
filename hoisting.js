9b1e44e4c5a4e38940b1c1894434a47f2d4af1f4
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-07-13 23:51:43 +0530

    Function Invocation and variable Environment

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,12 @@
-var x = 7;
-function getName() {
-  console.log("Namaste JS");
-}
-getName();
+var x = 1;
+a();
+b();
 console.log(x);
+function a() {
+  var x = 10;
+  console.log(x);
+}
+function b() {
+  var x = 100;
+  console.log(x);
+}
