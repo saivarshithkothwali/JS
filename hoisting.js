0ece8fcecae7ece8f9a8769858ff13d058273572
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-07-16 11:30:22 +0530

    Understanding Global Object ie window

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,7 @@
-var x = 1;
-a();
-b();
-console.log(x);
-function a() {
-  var x = 10;
-  console.log(x);
+var a = 10;
+function f() {
+  var x = 1;
 }
-function b() {
-  var x = 100;
-  console.log(x);
-}
+console.log(this.a);
+console.log(window.a);
+console.log(a);
